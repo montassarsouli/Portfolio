775dca0b6652f1f4555acc4e63fd0c5bc5dcbafe
Montassar <elsoulimontassar@gmail.com>
2024-02-19 16:43:12 +0100

    update1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <div className="container">
         <Hero />
+        <Skills />
       </div>
     </>
   );
---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "./image/img11.png",
+    skills: [
+      { skill: "HTML5", pourcentage: "80%" },
+      { skill: "CSS3 (Sass/SCSS)", pourcentage: "90%" },
+      { skill: "Javascript", pourcentage: "75%" },
+      { skill: "React.js", pourcentage: "80%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./image/img12.png",
+    skills: [
+      { skill: "Node.js", pourcentage: "70%" },
+      { skill: "Express.js", pourcentage: "65%" },
+    ],
+  },
+  {
+    title: "Tools",
+    icon: "./image/img13.png",
+    skills: [
+      { skill: "Git & Github", pourcentage: "85%" },
+      { skill: "Visual Studio Code", percentage: "75%" },
+      { skill: "Webpack & Vite", pourcentage: "60%" },
+      { skill: "Responsive Design", percentage: "70%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./image/img14.png",
+    skills: [
+      { skill: "Problem Solving", percentage: "80%" },
+      { skill: "Team Work", percentage: "85%" },
+      { skill: "Comunication", percentage: "85%" },
+    ],
+  },
+];
